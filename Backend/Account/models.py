8ea930b856eb79f5e67511bcfc1df6af27c0f288
savitrasapre <savitrasapre@gmail.com>
2025-02-14 15:10:22 -0500

    Adding models as well as few database items

--- Backend/Account/models.py
+++ Backend/Account/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=50)
+    password_hash = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.username
+
+class Token(models.Model):
+    id = models.AutoField(primary_key=True)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    token = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.token
+
+class WatchlistItems(models.Model):
+    id = models.AutoField(primary_key=True)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    symbol_name = models.CharField(max_length=50)
+    symbol_details = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
