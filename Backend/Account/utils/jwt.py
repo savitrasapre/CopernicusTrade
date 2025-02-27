3eb28b7f7b65d21f673cd692be9299b94a63777d
savitrasapre <savitrasapre@gmail.com>
2025-02-27 16:42:05 -0500

    Adding actual jwt payload

--- Backend/Account/utils/jwt.py
+++ Backend/Account/utils/jwt.py
@@ -1,12 +1,12 @@
 import jwt
+from datetime import datetime
 from django.conf import settings
 
 
 def generate_token(user):
     payload = {
-        'user_id': user.id,
-        'email': user.email,
-        'username': user.username,
+        #'iat': datetime.now(),
+        #'exp': datetime.now() + settings.JWT_EXPIRATION_DELTA,
     }
     token = jwt.encode(payload, settings.SECRET_KEY, algorithm='HS256')
     print(token)
--- Backend/CopernicusFE/settings.py
+++ Backend/CopernicusFE/settings.py
@@ -112,6 +112,7 @@ USE_I18N = True
 
 USE_TZ = True
 
+JWT_EXPIRATION_DELTA = 60 * 60 * 24 * 7
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
