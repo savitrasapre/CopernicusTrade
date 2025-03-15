be6cd169c2903cf37aa5ad709a331f4bd5bb10e6
Savitra Sapre <savitrasapre@gmail.com>
2025-03-14 22:00:51 -0400

    Fixing settings celery broker url

--- Backend/CopernicusFE/settings.py
+++ Backend/CopernicusFE/settings.py
@@ -144,4 +144,4 @@ STATIC_ROOT =  os.path.join(BASE_DIR, "static")
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 #Celery broker URL
-CELERY_BROKER_URL = "redis://localhost:6379/0" 
+CELERY_BROKER_URL = "redis://redis:6379/0" 
