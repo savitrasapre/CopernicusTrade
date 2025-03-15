86a319fe270ff48def45d067d3b7d9e57d04f654
Savitra Sapre <savitrasapre@gmail.com>
2025-03-14 21:50:39 -0400

    Adding changes to celery worker and entrypoint

--- Backend/entrypoint.sh
+++ Backend/entrypoint.sh
@@ -1,30 +1,32 @@
 #!/bin/sh
 #Make sure the end of line sequence is LF for this file.
-
-python manage.py makemigrations
-python manage.py migrate
-python manage.py createcachetable
-
-# Check if the superuser already exists before creating it
-echo "Checking for existing superuser..."
-python manage.py shell -c "
-from django.contrib.auth import get_user_model;
-User = get_user_model();
-exists = User.objects.filter(is_superuser=True).exists();
-exit(exists)
-"
-
-if [ $? -eq 1 ]; then
-    echo "Superuser already exists"
+if ["CELERY_WORKER" = "true"]; then
+    celery -A CopernicusFE worker --loglevel=info
 else
-    echo "Superuser does not exist"
-    if [ "$DJANGO_SUPERUSER_USERNAME" ]; then
-        echo "Creating superuser..."
-        python manage.py createsuperuser \
-            --noinput \
-            --username $DJANGO_SUPERUSER_USERNAME \
-            --email $DJANGO_SUPERUSER_EMAIL
+    python manage.py makemigrations
+    python manage.py migrate
+    python manage.py createcachetable
+
+    # Check if the superuser already exists before creating it
+    echo "Checking for existing superuser..."
+    python manage.py shell -c "
+    from django.contrib.auth import get_user_model;
+    User = get_user_model();
+    exists = User.objects.filter(is_superuser=True).exists();
+    exit(exists)
+    "
+
+    if [ $? -eq 1 ]; then
+        echo "Superuser already exists"
+    else
+        echo "Superuser does not exist"
+        if [ "$DJANGO_SUPERUSER_USERNAME" ]; then
+            echo "Creating superuser..."
+            python manage.py createsuperuser \
+                --noinput \
+                --username $DJANGO_SUPERUSER_USERNAME \
+                --email $DJANGO_SUPERUSER_EMAIL
+        fi
     fi
-fi
 
 $@
