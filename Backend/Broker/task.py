d4de2913a0f816bcdceaeaee2e343396e7fee394
Savitra Sapre <savitrasapre@gmail.com>
2025-03-17 18:11:10 -0400

    Adding task to broker

--- Backend/Broker/task.py
+++ Backend/Broker/task.py
@@ -1,5 +1,9 @@
 from celery import shared_task
 from datetime import datetime
+from ..Broker.models import SymbolData
+import logging
+
+logger = logging.getLogger(__name__)
 
 @shared_task
 def hello_world():
@@ -9,3 +13,27 @@ def hello_world():
 @shared_task
 def periodic_print():
     print("PERIODIC TASK - ", datetime.now())
+
+
+@shared_task
+def get_chart_bars():
+    try:
+        import yfinance as yf
+        print("-- Getting chart bars --")
+        symbols = "AAPL"
+        symbol_data = yf.Ticker(symbols)
+        data_history = symbol_data.history(period="1mo", interval="1d")
+        json_data = [
+            {'x': index.strftime('%Y-%m-%d'), 'o': row['Open'], 'h': row['High'], 'l': row['Low'], 'c': row['Close']}
+            for index, row in data_history.iterrows()
+        ]
+        if json_data:
+            logger.info("-- Inserting in the database --")
+            SymbolData.objects.update_or_create(
+                symbol_name=symbols,
+                defaults={'market_data': json_data, 'broker_name': 'yf', 'symbol_name': symbols}
+            )
+        else:
+            logger.warning("Yfinance data not retrieved")
+    except Exception as e:
+        logger.error(f"Error fetching chart bars: {e}")
