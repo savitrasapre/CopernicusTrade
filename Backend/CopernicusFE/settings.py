8cdb420c5693e3c1997d4c3c0e8e5165556e0955
Savitra Sapre <savitrasapre@gmail.com>
2025-03-21 19:32:34 -0400

    Putting debug as false manually

--- Backend/CopernicusFE/settings.py
+++ Backend/CopernicusFE/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.environ.get("DEBUG", default=0))
+#DEBUG = bool(os.environ.get("DEBUG", default=0))
+DEBUG=False
 
 #ALLOWED_HOSTS = []
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS","127.0.0.1").split(",")
