480b07e3033b2fae3352a37cec19404b3d124403
savitrasapre <savitrasapre@gmail.com>
2025-03-27 10:41:37 -0400

    Fixing Register View path

--- Backend/Account/urls.py
+++ Backend/Account/urls.py
@@ -5,6 +5,6 @@ from .views import AccountView, LoginView, RegisterView
 urlpatterns = [
     path('', AccountView.as_view(), name='index'),
     path("login/<str:username>/<str:password_hash>", LoginView.as_view(), name='login'),
-    path("register/<str:username>/<str:email>/<str:password_hash>", RegisterView.as_view().register, name='register'),
+    path("register/<str:username>/<str:email>/<str:password_hash>", RegisterView.as_view(), name='register'),
     path("<str:echo_str>/echo", views.echo, name='echo')
 ]
